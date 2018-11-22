31868261a40f16c26c6f2d0bb6ae4c85241f603c
vishistv <vishistvvishy@gmail.com>
2018-11-23 02:49:53 +0530

    Update ScoreListActivity.java
    Save Button now works, with dynamically adding scores to the List as and when the "SAVE SCORE" is clicked. Added SQLite as database. Also deletion of scores from the database/list can also be added in the future.

--- app/src/main/java/com/lal/android/out/ScoreListActivity.java
+++ app/src/main/java/com/lal/android/out/ScoreListActivity.java
@@ -1,53 +1,25 @@
 package com.lal.android.out;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.ArrayAdapter;
-import android.widget.LinearLayout;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
-import android.widget.TextView;
-import java.util.ArrayList;
-//import static com.lal.android.out.R.id.rootView;
+
 
 public class ScoreListActivity extends AppCompatActivity {
+
+    SQLiteHandler db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score_list);
 
+        db = new SQLiteHandler(this);
 
-        ArrayList<Score> scoreList = new ArrayList<Score>();
+//        db.deleteAllMatch(); deletes everything in the database
 
-        scoreList.add(new Score("87 Runs","in 5.4 Overs"));
-        scoreList.add(new Score("112 Runs","in .8.5 Overs"));
-        scoreList.add(new Score("12 Runs","in 3 Overs"));
-        scoreList.add(new Score("56 Runs","in 5 Overs"));
-        scoreList.add(new Score("76 Runs","in 7 Overs"));
-        scoreList.add(new Score("100 Runs","in 10 Overs"));
-        scoreList.add(new Score("99 Runs","in 9 Overs"));
-        scoreList.add(new Score("23 Runs","in 3.5 Overs"));
-        scoreList.add(new Score("44 Runs","in 5 Overs"));
-        scoreList.add(new Score("50 Runs","in 5 Overs"));
-        scoreList.add(new Score("178 Runs","in 10.3 Overs"));
-        scoreList.add(new Score("21 Runs","in 7 Overs"));
-        scoreList.add(new Score("11 Runs","in 3.2 Overs"));
-
-
-       // LinearLayout rootView = (LinearLayout)findViewById(R.id.rootView) ;
-
-//        for(int index = 0; index < scoreList.size(); index++) {
-//
-//            TextView scoreView = new TextView(this);
-//            scoreView.setText(scoreList.get(index));
-//            rootView.addView(scoreView);
-//        };
-
-        ScoreAdapter adapter = new ScoreAdapter(this, scoreList);
-
+        ScoreAdapter adapter = new ScoreAdapter(this, db.getMatch());
         ListView listView = (ListView) findViewById(R.id.list);
-
         listView.setAdapter(adapter);
 
 
